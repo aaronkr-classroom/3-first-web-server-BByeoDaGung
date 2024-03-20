b451e146c3da8b533bdfdab992f6512bdf80f89e
BByeoDaGung <163230772+BByeoDaGung@users.noreply.github.com>
2024-03-20 06:21:23 +0000

    5끝!!!!

--- lesson-5/5.6.TODO.js
+++ lesson-5/5.6.TODO.js
@@ -1,22 +1,40 @@
-// listing5.5.js
 // listing5.6.js
 
 /**
  * listing5.6.js
- * 간단한 가우팅 (p. 97)
+ * 간단한 라우팅 (p. 97)
  */
 
 // 요청에 따른 라우트의 매핑 정의
-const routeRespMap;
+const routeRespMap = {
+    "/": "<h1>Home Page</h1><p>Welcome to my page!</p>",
+    "/about": "<h1>About Page</h1><p>About me</p>",
+    "/info": "<h1>Info Page</h1><p>Personal Info</p>",
+    "/contact": "<h1>Contact Page</h1><p>Contact me!</p>",
+};
 
-// listing5.5.js에서 (p. 96)
-const port;
+const port = 3000,
+    http = require('http'),
+    httpStatus = require('http-status-codes'),
+    app = http.createServer();
 
-// 요청 라우트가 정의된 맵에 있는지 체크
+// 요청 핸들러 등록
+app.on('request', (req, res) => {
+    res.writeHead(httpStatus.OK, {
+        "Content-Type": "text/html",
+    });
 
-// <<< 나머진 서버 코드 입력 하십시오 >>>
+    // 요청 라우트가 정의된 맵에 있는지 체크
+    if (routeRespMap[req.url]) {
+        res.end(routeRespMap[req.url]); // 지연 없이 바로 응답을 보내도록 수정
+    } else {
+        res.end("<h1>404</h1><p>Where are you?</p>"); // 404 응답을 지연 없이 바로 보내도록 수정
+    }
+});
 
-// listing5.7.js에서 (p. 98)
-// 수동으로 응답에 지연을 걸기 위한 코드 감싸기
-// setTimeout(() => res.end(routeRespMap[req.url]), 2000); 
-// list5.6 res.end() 코드를 이 줄로 대체하여 서버 처리 지연을 시뮬레이트하십시오.
+// 서버 리스닝 및 콘솔 출력
+if (process.env.NODE_ENV !== 'test') {
+    app.listen(port, () => {
+        console.log(`Server is running at http://localhost:${port}`);
+    });
+}
